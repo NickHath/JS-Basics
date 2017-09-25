78ad606d674679f3d2c625767f6830800fcb32c6
Nick Hathaway <Nicholashathaway@gmail.com>
2017-09-25 16:22:32 -0600

    update answer format

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -38,7 +38,7 @@ var name = 'Tyler';
 
 //What is the difference between arguments and parameters?
 
-  Arguments are passed into functions, parameters are local variables specified in a function declaration
+  /* Arguments are passed into functions, parameters are local variables specified in a function declaration */
 
 
 //////////////////PROBLEM 5////////////////////
@@ -47,11 +47,8 @@ var name = 'Tyler';
 
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
 
-
-  falsy values: 0, undefined, null, NaN, "", false
-  you could evaluate it in an if statement
-
-
+  /* falsy values: 0, undefined, null, NaN, "", false
+  you could evaluate it in an if statement */
 
 //////////////////PROBLEM 6////////////////////
 
